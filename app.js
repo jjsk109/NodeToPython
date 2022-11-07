900d9e3a3216159e338337bd431d5857b23d4189
장익순(NickSoon) <39641848+jjsk109@users.noreply.github.com>
2022-11-07 19:19:26 +0900

    2차원 배열 넣는 방법 공부

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import child_process from 'child_process';
 
-const child = child_process.spawn('python', ['app.py',1,5,"hello"]);
+const child = child_process.spawn('python', ['1_Base.py',1,5,"hello"]);
 
 
 child.stdout.on('data', (data) => {
@@ -8,5 +8,22 @@ child.stdout.on('data', (data) => {
 });
 
 child.stderr.on('data', function(data) {
+    console.log(data.toString());
+});
+
+// 2차원 배열을 전송하게 되면?
+const data = 
+[
+    '2_2Darray.py',
+    [20,30,50,50,60,80,10,21,43,53,36,543,5,53,63,67,345,36,754,546,45,456,45,645,645,645,6,456,47,32,7,257,256,27,5,6,2457,25,7,27,245,4],
+    [20,30,50,50,60,80,10,21,43,53,36,543,5,53,63,67,345,36,754,546,45,456,45,645,645,645,6,456,47,32,7,257,256,27,5,6,2457,25,7,27,245,4],
+    [20,30,50,50,60,80,10,21,43,53,36,543,5,53,63,67,345,36,754,546,45,456,45,645,645,645,6,456,47,32,7,257,256,27,5,6,2457,25,7,27,245,4]
+]
+
+const child2 = child_process.spawn('python', data);
+child2.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+});
+child2.stderr.on('data', function(data) {
     console.log(data.toString());
 });

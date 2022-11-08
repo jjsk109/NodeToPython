231832ce21fffaaea9390fe787b44f3ed9ffd1dd
장익순(NickSoon) <39641848+jjsk109@users.noreply.github.com>
2022-11-08 09:22:55 +0900

    json 데이터 for문 돌리기

--- 3_JSON.py
+++ 3_JSON.py
@@ -5,23 +5,28 @@ import json
 def save_value(value):
     
     # value , 기준으로 배열로 만들기
-    print('=======================')
+    print('=======================1')
     # string 을 json 으로 변환
     value = json.loads(value)
     print(value)
-    print('=======================')
+    print('=======================2')
     # 엑셀 파일 불러오기
     wb = load_workbook('test.xlsx')
     wsArr = wb.sheetnames
     ws = wb[wsArr[0]]
-    print((len(value)))
-    for i in range(1,len(value)):
-        print(i)
-        d = value[i].split(',')
-        ws.append(d)
+    print('=======================3')
+    
+    for arr in value:
+        print(arr)
+        for key,val in arr.items():
+            print(key,val)
+            
+        
+        
+        
    
     
-    wb.save('test3.xlsx')
+    wb.save('test4.xlsx')
     # 엑셀 파일 닫기
     wb.close()
     # 셀의 값을 반환
